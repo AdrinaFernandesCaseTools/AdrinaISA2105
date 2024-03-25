ec1fe9aaa7b76f108fe4b394137ebbe56bf0850a
Admin <Admin@DESKTOP-H3G0PQ1>
2024-03-25 05:04:59 -0800

    commit

--- ISA2105/src/Programs/DogRBCA22105Test.java
+++ ISA2105/src/Programs/DogRBCA22105Test.java
@@ -10,10 +10,6 @@ import org.junit.Test;
 
 public class DogRBCA22105Test {
 
-	public DogRBCA22105Test(String string, String string2) {
-		// TODO Auto-generated constructor stub
-	}
-
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -33,8 +29,8 @@ public class DogRBCA22105Test {
 	@Test
 	public void testDog() {
         // Create two instances of the Dog class
-		DogRBCA22105Test dog1 = new DogRBCA22105Test ("Buddy", "Labrador");
-		DogRBCA22105Test dog2 = new DogRBCA22105Test ("Max", "German Shepherd");
+		DogRBCA22105 dog1 = new DogRBCA22105("Buddy", "Labrador");
+		DogRBCA22105 dog2 = new DogRBCA22105("Max", "German Shepherd");
 
         // Modify the attributes using setter methods
         dog1.setName("Rocky");
@@ -50,29 +46,7 @@ public class DogRBCA22105Test {
         assertEquals("Bella", dog2.getName());
         assertEquals("Beagle", dog2.getBreed());
     
-	}
 
-	private void setBreed(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void setName(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private Object getBreed() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private Object getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+}
 	
-		
-	
-
 }
