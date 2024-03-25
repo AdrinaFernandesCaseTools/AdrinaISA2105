f9cbfe88d56da067137cc738963cc4c02eb54251
Admin <Admin@DESKTOP-H3G0PQ1>
2024-03-25 00:45:28 -0800

    commit

---
+++ ISA2105/src/Programs/PersonRBCA22105.java
@@ -0,0 +1,21 @@
+package Programs;
+
+public class PersonRBCA22105 {
+	private String name;
+    private int age;
+
+    public PersonRBCA22105 (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+
