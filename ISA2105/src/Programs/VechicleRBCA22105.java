55af9d8a74495a8baf59708b75bd674c5ff2b4a4
Admin <Admin@DESKTOP-H3G0PQ1>
2024-03-25 05:28:58 -0800

    commit

---
+++ ISA2105/src/Programs/VechicleRBCA22105.java
@@ -0,0 +1,19 @@
+package Programs;
+
+public class VechicleRBCA22105 {
+	private int horsepower;
+
+	 public void VehicleRBCA22105 (int horsepower) {
+	        this.horsepower = horsepower;
+	    }
+
+	    public void drive() {
+	        System.out.println("Vehicle is being driven");
+	    }
+
+	    public int getHorsepower() {
+	        return horsepower;
+	    }
+}
+
+
