bcc7bca28a8c9daeaf36259687d35079061965d2
Admin <Admin@DESKTOP-H3G0PQ1>
2024-03-25 04:19:34 -0800

    commit

---
+++ ISA2105/src/Programs/DogRBCA22105.java
@@ -0,0 +1,29 @@
+package Programs;
+
+public class DogRBCA22105 {
+	private String name;
+    private String breed;
+
+    public DogRBCA22105(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
+
+
